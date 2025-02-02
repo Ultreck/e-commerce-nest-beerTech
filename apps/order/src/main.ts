5ce673657aa0bb2327508bed46ff83f0ae5a4c31
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-02-02 22:51:42 +0100

    feat: continue working on the microservice implementation

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { OrderModule } from './order.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(OrderModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(OrderModule, {
+    transport: Transport.TCP,
+   options: {
+    host: 'localhost',
+    port: parseInt(process.env.USER_SERVICE_PORT || '3002', 10),
+   },
+  });
+  await app.listen();
 }
 bootstrap();
--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'apps/user/src/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [ User],
+      synchronize: true, //set to false in production
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
